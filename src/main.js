31ac65da7662d3de504a7bd0abfd3df254734385
codeWorkspace1 <codeworkspace1@gmail.com>
2024-09-22 10:45:18 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-console.log(window.scrollY);
   if(window.scrollY > headerHeight){
     header.classList.add('header--dark');
   }else {
     header.classList.remove('header--dark');
   }
+});
+
+//Hmoe 섹션을 아래로 스크롤시 투명하게 처리함 (offsetHeright 높이가져옴.)
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  console.log(1 - window.scrollY /homeHeight);
+  home.style.opacity = 1 - window.scrollY /homeHeight;
 });
